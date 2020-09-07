0e9a376c6ceadecb0a7152bf5861da92e9e568eb
YamiOdymel <yamiodymel@gmail.com>
2020-09-07 18:20:35 +0800

    TestTimeout

--- trillium_test.go
+++ trillium_test.go
@@ -14,6 +14,22 @@ func TestUint64(t *testing.T) {
 	assert.Greater(len(strconv.Itoa(int(num))), 10)
 }
 
+func TestTimeout(t *testing.T) {
+	assert := assert.New(t)
+
+	var i int
+	for {
+		if i > 256 {
+			break
+		}
+		i++
+
+		num, err := New(DefaultConfig()).Generate()
+		assert.NoError(err)
+		assert.Greater(len(strconv.Itoa(int(num))), 10)
+	}
+}
+
 func BenchmarkUint64(b *testing.B) {
 	t := New(DefaultConfig())
 	b.ReportAllocs()
